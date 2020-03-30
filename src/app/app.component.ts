d20b7ef2c15b31062ddc8f78a004ac8d6b544848
Chirag Sane <chiragsane333@gmail.com>
2020-03-30 19:01:29 +0530

    add responsive homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, Renderer2, OnInit, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'profile';
+export class AppComponent implements OnInit, AfterViewInit {
+  name = 'ChiragSane';
+  links = [
+    'About Me',
+  ];
+  mentions = [
+    'Angular',
+    'Git',
+    'Love',
+  ];
+  visibleMention: string[];
+  visibleMentionIndex = 0;
+  @ViewChild('mentionIcon') mentionIcon: ElementRef;
+  constructor(private renderer: Renderer2) { }
+  ngOnInit(): void {
+    this.visibleMention = this.mentions[this.visibleMentionIndex].split('');
+    setInterval(() => {
+      this.renderer.setStyle(this.mentionIcon.nativeElement, 'opacity', 0);
+      this.applyDeleteEffect();
+    }, 4000);
+  }
+  ngAfterViewInit(): void {
+    this.showMentionIcon();
+  }
+  applyDeleteEffect() {
+    this.visibleMention.pop();
+    this.visibleMention.length > 0
+      ? setTimeout(() => this.applyDeleteEffect(), 80)
+      : this.changeVisibleMention();
+  }
+  changeVisibleMention() {
+    this.visibleMentionIndex < this.mentions.length - 1
+      ? this.visibleMentionIndex++
+      : this.visibleMentionIndex = 0;
+    this.applyTypeEffect(this.mentions[this.visibleMentionIndex].split(''));
+  }
+  applyTypeEffect(newMention: string[]) {
+    const char = newMention.shift();
+    if (typeof char === 'string') {
+      this.visibleMention.push(char);
+      setTimeout(() => this.applyTypeEffect(newMention), 80);
+    } else {
+      this.showMentionIcon();
+    }
+  }
+  showMentionIcon() {
+    this.renderer.removeStyle(this.mentionIcon.nativeElement, 'background-image');
+    this.renderer.setStyle(this.mentionIcon.nativeElement, 'opacity', 1);
+    this.renderer.setStyle(
+      this.mentionIcon.nativeElement,
+      'background-image',
+      `url("assets/icons/${this.visibleMention.join('').toLowerCase()}.svg")`
+    );
+  }
 }
